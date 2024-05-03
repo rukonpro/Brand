64d99010982c39a1ca46db7023ae23955e45ecd7
rukonpro <rukon.js@gmail.com>
2024-05-03 15:39:21 +0600

    profile update form design

--- app/profiles/myAccount/page.js
+++ app/profiles/myAccount/page.js
@@ -4,13 +4,14 @@ import Image from "next/image";
 import profilePhoto from "@/public/images/profile-photo-1.webp";
 import CoverPhoto from "@/public/images/cloth-image-1.jpg";
 import CameraIcon from "@/public/images/camera-icon.svg";
-
+import EditIcon from "@/public/images/edit-2-svgrepo-com.svg";
+import SaveIcon from "@/public/images/save-floppy-svgrepo-com.svg";
 const MyAccount = () => {
 
     const [selectedProfilePicture, setSelectedProfilePicture] = useState(null);
     const [selectedCoverPhoto, setSelectedCoverPhoto] = useState(null);
-
     const [hovered, setHovered] = useState(false);
+    const [edit,setEdit]=useState(false);
     const handleProfilePhotoChange = (event) => {
         setSelectedProfilePicture(URL.createObjectURL(event.target.files[0]));
     };
@@ -79,8 +80,6 @@ const MyAccount = () => {
                             style={{display: 'none'}}
                         />
                     </label>
-
-
                 </div>
 
 
@@ -93,6 +92,111 @@ const MyAccount = () => {
                         className="w-full h-60 object-cover"
                     />
                 </div>
+            </div>
+
+
+            <div className="border-t-2 mt-8">
+                <div className="flex justify-between items-center">
+                    <h1 className="text-xl font-bold text-gray-600 py-5 px-3 md:px-0">Personal details</h1>
+                    <button type="button"
+                            onClick={()=>setEdit(state=>!state)}
+                            className="border-2 border-[#3e8cfe] hover:bg-blue-300 hover:text-white text-gray-500 font-bold  px-3 py-1 rounded-lg flex items-center gap-2">
+
+                        {
+                            <Image src={!edit?EditIcon:SaveIcon} height={24} alt="eidt icon"/>
+                        }
+                        {!edit?
+                            "Edit now":"Save Change"}
+                    </button>
+                </div>
+
+                <div className="grid sm:grid-cols-4 md:grid-cols-4 gap-4  px-3 md:px-0">
+                    <div className="col-span-2">
+                        <label htmlFor="first_name" className="text-lg">First Name:</label>
+                        <br/>
+                        <input type="text" id="first_name" name="first_name" placeholder="First Name"
+                               className="px-3 py-2 rounded-lg mt-1  w-full"/>
+                    </div>
+                    <div className="col-span-2">
+                        <label htmlFor="last_name" className="text-lg">Last Name:</label>
+                        <br/>
+                        <input type="text" id="last_name" name="last_name" placeholder="Last Name"
+                               className="px-3 py-2 rounded-lg mt-1  w-full"/>
+                    </div>
+                    <div>
+                        <label htmlFor="gender" className="text-lg">Gender:</label>
+                        <br/>
+                        <select id="gender" name="gender"
+                                className="px-3 py-2 rounded-lg mt-1 w-full bg-white">
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Others">Others</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label htmlFor="date_of_birth" className="text-lg">Date of birth:</label>
+                        <br/>
+                        <input type="date" id="date_of_birth" name="date_of_birth"
+                               className="px-3 py-2 rounded-lg mt-1  w-full"/>
+                    </div>
+
+                    <div className="col-span-2">
+                        <label htmlFor="phone_number" className="text-lg">Phone Number:</label>
+                        <br/>
+                        <input type="text" id="phone_number" name="phone_number"
+                               placeholder="Phone Number"
+                               className="px-3 py-2 rounded-lg mt-1  w-full"/>
+                    </div>
+
+                    <div className="col-span-2">
+                        <label htmlFor="Country" className="text-lg">Country:</label>
+                        <br/>
+                        <input type="text" id="Country" name="Country"
+                               placeholder="Country"
+                               className="px-3 py-2 rounded-lg mt-1  w-full"/>
+                    </div>
+                    <div className="col-span-2">
+                        <label htmlFor="house_or_office_number" className="text-lg">House/Office number:</label>
+                        <br/>
+                        <input type="text" id="house_or_office_number" name="house_or_office_number"
+                               placeholder="House/Office number"
+                               className="px-3 py-2 rounded-lg mt-1  w-full"/>
+                    </div>
+                    <div className="col-span-2">
+                        <label htmlFor="street_name" className="text-lg">Street name:</label>
+                        <br/>
+                        <input type="text" id="street_name" name="street_name"
+                               placeholder="Street name"
+                               className="px-3 py-2 rounded-lg mt-1  w-full"/>
+                    </div>
+
+                    <div className="col-span-2">
+                        <label htmlFor="city" className="text-lg">City:</label>
+                        <br/>
+                        <input type="text" id="city" name="city"
+                               placeholder="City"
+                               className="px-3 py-2 rounded-lg mt-1  w-full"/>
+                    </div>
+
+                    <div>
+                        <label htmlFor="postal_code" className="text-lg">Postal code:</label>
+                        <br/>
+                        <input type="number" id="postal_code" name="postal_code"
+                               placeholder="Postal code"
+                               className="px-3 py-2 rounded-lg mt-1  w-full"/>
+                    </div>
+
+
+                    <div>
+                        <label htmlFor="state_province" className="text-lg">State/Province:</label>
+                        <br/>
+                        <input type="text" id="state_province" name="state_province"
+                               placeholder="State/Province"
+                               className="px-3 py-2 rounded-lg mt-1  w-full"/>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
