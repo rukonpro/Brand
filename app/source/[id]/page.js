1064ee8f624a34dd1ee5730172546fe410d948de
rukonpro <rukon.js@gmail.com>
2024-05-20 23:03:39 +0600

    remove nav

--- app/source/[id]/page.js
+++ app/source/[id]/page.js
@@ -6,10 +6,7 @@ import Footer from "@/app/components/Footer/Footer";
 const Page = ({params}) => {
     return (
         <div>
-            <Navbar/>
-            <Nav/>
             <h1>source {params.id}</h1>
-            <Footer/>
         </div>
     );
 };
