0a983a420d28ceb964b2bb53e66660e96ca4021f
rukonpro <rukon.js@gmail.com>
2024-05-24 21:27:16 +0600

    api conneted product and single product and details

--- app/page.js
+++ app/page.js
@@ -6,11 +6,14 @@ import OurExtraServices from "./components/OurExtraServices/OurExtraServices";
 import SuppliersByRegion from "./components/SuppliersByRegion/SuppliersByRegion";
 import Nav from "@/app/components/navbar/nav";
 import SourcesProductsLayout from "./components/SourcesProductsLayout/SourcesProductsLayout";
+import API_BASE_URL from "@/lib/API_BASE_URL/API_BASE_URL";
 
 
 
 export default function Home() {
-
+    if (!API_BASE_URL) {
+        return null
+    }
     return (
         <>
             <Nav />
