c5538c20fdfb2d13f93e677149d21d2629b7786f
rukonpro <rukon.js@gmail.com>
2024-05-24 22:30:21 +0600

    try catch use

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 module.exports = {
     images: {
         domains: ['https://rukonpro.imgbb.com', 'i.ibb.co', "adminapi.applegadgetsbd.com", "chenabazar.com"],
-    },
-    env: {
-        NEXT_PUBLIC_BASE_API_URL: process.env.NEXT_PUBLIC_BASE_API_URL,
-    },
+    }
+    
 }
