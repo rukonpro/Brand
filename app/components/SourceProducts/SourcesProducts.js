c2bb7ede987fe7bf9541a5c64919b6b0119d3842
rukonpro <rukon.js@gmail.com>
2024-05-24 21:21:06 +0600

    api conneted product and single product and details

--- app/components/SourceProducts/SourcesProducts.js
+++ app/components/SourceProducts/SourcesProducts.js
@@ -3,10 +3,15 @@ import SourceCard from "@/app/components/SourceProducts/SourceCard";
 import Link from "next/link";
 import SourceProductCard from "@/app/components/SourceProducts/SourceProductCard";
 import getProducts from '@/lib/product/getAllProducts';
+import API_BASE_URL from '@/lib/API_BASE_URL/API_BASE_URL';
 
 
 
 const SourcesProducts = async ({ category }) => {
+
+    if (API_BASE_URL) {
+        return null;
+    }
     const products = await getProducts()
 
 
