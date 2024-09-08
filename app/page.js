940ba610183b9aaba5bd057dd3cab91508c6bd22
rukonpro <rukon.js@gmail.com>
2024-09-08 18:35:22 +0600

    database url add

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Banner from "./components/Banner/Banner";
 import Offers from "./components/offers/Offers";
-// import Requests from "./components/requests/Requests";
+import Requests from "./components/requests/Requests";
 import RecommendedItems from "./components/RecommendedItems/RecommendedItems";
 import OurExtraServices from "./components/OurExtraServices/OurExtraServices";
 import SuppliersByRegion from "./components/SuppliersByRegion/SuppliersByRegion";
 import Nav from "@/app/components/navbar/nav";
-// import SourcesProductsLayout from "./components/SourcesProductsLayout/SourcesProductsLayout";
+import SourcesProductsLayout from "./components/SourcesProductsLayout/SourcesProductsLayout";
 import {Suspense} from "react";
 import Loading from "@/app/loading";
 
@@ -19,10 +19,10 @@ export default function Home() {
             <Suspense fallback={<Loading/>}>
                 <Offers />
             </Suspense>
-            {/*<Suspense fallback={<Loading/>}>*/}
-            {/*    <SourcesProductsLayout />*/}
-            {/*</Suspense>*/}
-            {/*<Requests />*/}
+            <Suspense fallback={<Loading/>}>
+                <SourcesProductsLayout />
+            </Suspense>
+            <Requests />
            <Suspense fallback={<Loading/>}>
                <RecommendedItems />
            </Suspense>
