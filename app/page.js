f3ec4f93259d83448b93e8de1c1150ba4e98514e
rukonpro <rukon.js@gmail.com>
2024-09-09 23:29:15 +0600

    update

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
-// import Banner from "./components/Banner/Banner";
-// import Offers from "./components/offers/Offers";
-// import Requests from "./components/requests/Requests";
-// import RecommendedItems from "./components/RecommendedItems/RecommendedItems";
-// import OurExtraServices from "./components/OurExtraServices/OurExtraServices";
-// import SuppliersByRegion from "./components/SuppliersByRegion/SuppliersByRegion";
+import Banner from "./components/Banner/Banner";
+import Offers from "./components/offers/Offers";
+import Requests from "./components/requests/Requests";
+import RecommendedItems from "./components/RecommendedItems/RecommendedItems";
+import OurExtraServices from "./components/OurExtraServices/OurExtraServices";
+import SuppliersByRegion from "./components/SuppliersByRegion/SuppliersByRegion";
 import Nav from "@/app/components/navbar/nav";
-// import SourcesProductsLayout from "./components/SourcesProductsLayout/SourcesProductsLayout";
-// import {Suspense} from "react";
-// import Loading from "@/app/loading";
+import SourcesProductsLayout from "./components/SourcesProductsLayout/SourcesProductsLayout";
+import {Suspense} from "react";
+import Loading from "@/app/loading";
 
 
 export default function Home() {
@@ -15,19 +15,19 @@ export default function Home() {
     return (
         <>
             <Nav />
-           {/* <Banner />*/}
-           {/* <Suspense fallback={<Loading/>}>*/}
-           {/*     <Offers />*/}
-           {/* </Suspense>*/}
-           {/* <Suspense fallback={<Loading/>}>*/}
-           {/*     <SourcesProductsLayout />*/}
-           {/* </Suspense>*/}
-           {/* <Requests />*/}
-           {/*<Suspense fallback={<Loading/>}>*/}
-           {/*    <RecommendedItems />*/}
-           {/*</Suspense>*/}
-           {/* <OurExtraServices />*/}
-           {/* <SuppliersByRegion />*/}
+            <Banner />
+            <Suspense fallback={<Loading/>}>
+                <Offers />
+            </Suspense>
+            <Suspense fallback={<Loading/>}>
+                <SourcesProductsLayout />
+            </Suspense>
+            <Requests />
+           <Suspense fallback={<Loading/>}>
+               <RecommendedItems />
+           </Suspense>
+            <OurExtraServices />
+            <SuppliersByRegion />
         </>
     );
 }
