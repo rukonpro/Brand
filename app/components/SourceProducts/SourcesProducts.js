458f16e58607751a11487aa0b1e62b955a615c20
rukonpro <rukon.js@gmail.com>
2024-09-12 22:17:28 +0600

    category

--- app/components/SourceProducts/SourcesProducts.js
+++ app/components/SourceProducts/SourcesProducts.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import SourceCard from "@/app/components/SourceProducts/SourceCard";
 import Link from "next/link";
@@ -15,27 +16,27 @@ const SourcesProducts = async ({ category }) => {
     const products = await getProducts(params);
 
 
-
+// console.log(category);
 
     return (
 
         <div className="sm:px-3">
             <div className='max-w-[1200px] mx-auto mt-5'>
-                <div
-                    className='grid grid-cols-12  border-2 border-gray-200 sm:rounded-lg gap-[2px] bg-gray-200 overflow-hidden'>
+
+                <div className='grid grid-cols-12  border-2 border-gray-200 sm:rounded-lg gap-[2px] bg-gray-200 overflow-hidden'>
                     <div className="md:col-span-3 col-span-12 bg-white">
                         <SourceCard
                             id={category?.id}
                             photo={category?.photo}
-                            name={category?.name} />
+                            name={category?.name}/>
                     </div>
                     <div className='grid md:grid-cols-5 grid-cols-2 md:col-span-9 col-span-12 gap-[2px]'>
                         {
                             products?.data.map((product) => {
                                 return (
-                                    <div key={product.id} >
+                                    <div key={product.id}>
                                         <Link href={`/details/${product.id}`}>
-                                            <SourceProductCard product={product} />
+                                            <SourceProductCard product={product}/>
                                         </Link>
                                     </div>
                                 )
--- app/components/SourcesProductsLayout/SourcesProductsLayout.js
+++ app/components/SourcesProductsLayout/SourcesProductsLayout.js
@@ -20,6 +20,8 @@ const SourcesProductsLayout = async () => {
                     )
                 })
             }
+
+
         </div>
     );
 };
