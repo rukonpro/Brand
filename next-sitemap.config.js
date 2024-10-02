38aa1e8734fc02d8383696d4a626cbaf81ece677
rukonpro <rukon.js@gmail.com>
2024-10-03 02:13:06 +0600

    google search indexing

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://brand-rukon.vercel.app', // আপনার সাইটের URL এখানে দিন
+    generateRobotsTxt: true, // robots.txt জেনারেট করবে
+}
