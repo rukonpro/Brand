bfc5ac2f6c01cfb7d6d1c28cb82e1e4411a25ba2
rukonpro <rukon.js@gmail.com>
2024-10-17 01:30:05 +0600

    update details page

--- app/details/[id]/page.js
+++ app/details/[id]/page.js
@@ -17,7 +17,7 @@ import Navbar from "@/app/components/navbar/navbar";
 import Image from 'next/image';
 import Specifications from '@/app/components/Specifications/Specifications';
 import Footer from '@/app/components/Footer/Footer';
-import VariantCatd from '@/app/components/VariantCard/VariantCard';
+
 
 
 // export async function generateMetadata({ params }, parent) {
@@ -81,6 +81,9 @@ const Details = async ({ params }) => {
 
     const product = await getDetailsProduct({ id });
 
+
+
+
     return (
         <>
             <div className="sticky top-0 z-[20]">
@@ -88,7 +91,9 @@ const Details = async ({ params }) => {
             </div>
             <Nav />
 
-            <div className="max-w-[1200px] mx-auto md:px-3  pb-5">
+            <div className="max-w-[1200px] mx-auto md:px-3  pb-5 ">
+
+
 
                 <div className="flex justify-between items-center px-3 md:px-0">
                     <h1 className="text-2xl  font-bold text-gray-600 py-5 dark:text-slate-50">Details</h1>
@@ -99,8 +104,12 @@ const Details = async ({ params }) => {
                 </div>
                 <Suspense fallback={<Loader />}>
                     <div
-                        className="grid grid-cols-12 gap-4 md:border-2 p-3  md:bg-white md:rounded-lg dark:bg-slate-800 dark:border-slate-700">
+                        className="grid grid-cols-12 gap-4 md:border-2 p-3  md:bg-white md:rounded-lg dark:bg-slate-800 dark:border-slate-700 relative">
                         <div className="col-span-12 md:col-span-4">
+
+                            <div className='absolute right-3 top-3'>
+                                <SaveForLaterButton product={product?.data} />
+                            </div>
                             {product?.data?.variant?.length > 0 ?
                                 (<ImageChangeButton
 
@@ -139,148 +148,124 @@ const Details = async ({ params }) => {
                             </div>
                             <div className="pt-5">
                                 <h1 className="text-2xl font-bold">{product?.data?.name}</h1>
-                                {product?.data?.rating >= 0 && <p> Rating: {product?.data?.rating}</p>}
+                                <p> Rating: {product?.data?.rating}</p>
 
-                                <div className="pt-5">
+                                {product?.data?.brand?.name &&
+                                    <div className="grid grid-cols-12 pt-1 ">
+                                        <p className="col-span-4">Brand:</p>
+                                        <p className="col-span-8">{product?.data?.brand?.name}</p>
+                                    </div>}
+
+                                <div className='border-t border-slate-50 dark:border-slate-700 mt-6' />
+
+                                <div className="pt-5 ">
                                     {product?.data?.basePrice &&
-                                        <div className="grid grid-cols-12 border-b-2 border-b-gray-700/50 pb-1">
+                                        <div className="grid grid-cols-12  pb-1">
                                             <p className="col-span-4">Price:</p>
-                                            <p className="col-span-8">${product?.data?.basePrice}</p>
+                                            <p className="col-span-8 text-3xl text-blue-500">{product?.data?.basePrice > 0 ?
+                                                "$" + product?.data?.basePrice : "free"}</p>
                                         </div>
                                     }
-                                    {product?.data?.taxPercentage &&
-                                        <div className="grid grid-cols-12 border-b-2 border-b-gray-700/50 pb-1">
-                                            <p className="col-span-4">Tax percentage:</p>
-                                            <p className="col-span-8">{product?.data?.taxPercentage}%</p>
-                                        </div>
-                                    }
+
+                                    <div className="grid grid-cols-12 pb-1 pt-6">
+                                        <p className="col-span-4">Tax percentage:</p>
+                                        <p className="col-span-8">{product?.data?.taxPercentage > 0 ? product?.data?.taxPercentage + "%" : "Free"}</p>
+                                    </div>
+
 
                                     {product?.data?.deliveryFee &&
-                                        <div className="grid grid-cols-12 border-b-2 border-b-gray-700/50 pb-1">
+                                        <div className="grid grid-cols-12  pb-1">
                                             <p className="col-span-4">Delivery fee:</p>
-                                            <p className="col-span-8">${product?.data?.deliveryFee}</p>
+                                            <p className="col-span-8">{product?.data?.deliveryFee > 0 ? "$" + product?.data?.deliveryFee : "Free"}</p>
                                         </div>
                                     }
 
-                                    <div className="border-b-2 border-b-gray-700/50 pb-3">
 
-
-                                        {product?.data?.brand?.name &&
-                                            <div className="grid grid-cols-12 pt-1 ">
-                                                <p className="col-span-4">Brand:</p>
-                                                <p className="col-span-8">{product?.data?.brand?.name}</p>
-                                            </div>}
-
-
-
-                                        {product?.data?.design && <div className="grid grid-cols-12 pt-1">
-                                            <p className="col-span-4">Design:</p>
-                                            <p className="col-span-8">{product?.data?.design}</p>
-
-
-
-                                        </div>
-
-                                        }
-
-
+                                    {product?.data?.design && <div className="grid grid-cols-12 pt-1">
+                                        <p className="col-span-4">Design:</p>
+                                        <p className="col-span-8">{product?.data?.design}</p>
                                     </div>
+                                    }
 
-                                    <div className="b-1">
-                                        {product?.data?.protection && <div className="grid grid-cols-12 pt-1">
-                                            <p className="col-span-4 font-bold">Protection:</p>
+
+
+                                    {product?.data?.protection &&
+                                        <div className="grid grid-cols-12 pt-1">
+                                            <p className="col-span-4 ">Protection:</p>
                                             <p className="col-span-8">{product?.data?.protection}</p>
                                         </div>}
-                                        {
-                                            product?.data?.warranty &&
-                                            <div className="grid grid-cols-12 pt-1">
-                                                <p className="col-span-4 font-bold">Warranty:</p>
-                                                <p className="col-span-8">{product?.data?.warranty}</p>
-                                            </div>
-                                        }
-
-
-                                        {/* {
-                                            <div className=' border  border-slate-700/50 rounded  mt-5 p-3'>
-
-                                                <ul className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2  grid-cols-2 gap-5 py-5'>
-                                                    {
-                                                        product?.data?.variant?.map(variant => {
-                                                            return (
-                                                                <li key={variant?.id} className=' bg-slate-700 p-3 rounded-lg'>
-                                                                    <div className="w-full flex justify-center ">
-                                                                        <Image src={variant?.images?.[0]} width={100} height={100} alt={product?.data?.name} className='h-28 w-28 object-contain' />
-                                                                    </div>
-
-                                                                    <div>
-                                                                        <VariantCatd data={variant} />
-                                                                    </div>
-
-                                                                </li>
-                                                            )
-                                                        })
-                                                    }
-                                                </ul>
-                                            </div>
-                                        } */}
-
-                                    </div>
+                                    {
+                                        product?.data?.warranty &&
+                                        <div className="grid grid-cols-12 pt-1">
+                                            <p className="col-span-4 ">Warranty:</p>
+                                            <p className="col-span-8">{product?.data?.warranty}</p>
+                                        </div>
+                                    }
                                 </div>
+                                <div className='border-t border-slate-50 dark:border-slate-700 mt-6' />
+
+
+                                {
+                                    product?.data?.variant?.length > 0 &&
+
+                                    <ul className='grid grid-cols-5 gap-3 pt-5'>
+                                        {
+                                            product?.data?.variant?.map((variant) => {
+                                                return (
+                                                    <li key={variant?.id} className=' cursor-pointer border-2 border-blue-500 rounded-lg '>
+                                                        <div className='p-2'>
+                                                            <Image src={variant?.images?.[0]} height={100} width={100} alt={variant?.color}
+                                                                className='w-full object-contain'
+                                                            />
+                                                        </div>
+
+                                                        <div className='pb-2'>
+                                                            {variant?.price && <div className='grid grid-cols-2 hover:bg-slate-700 px-2'>
+                                                                <p className='col-span-1 text-sm'>Price</p>
+                                                                <p className='col-span-1 text-sm'>: ${variant?.price}</p>
+                                                            </div>}
+                                                            {variant?.color && <div className='grid grid-cols-2 hover:bg-slate-700 px-2'>
+                                                                <p className='col-span-1 text-sm'>Color</p>
+                                                                <p className='col-span-1 text-sm'>: {variant?.color}</p>
+                                                            </div>}
+                                                            {variant?.size && <div className='grid grid-cols-2 hover:bg-slate-700 px-2'>
+                                                                <p className='col-span-1 text-sm'>Size</p>
+                                                                <p className='col-span-1 text-sm'>: {variant?.size}</p>
+                                                            </div>}
+                                                            {variant?.storage && <div className='grid grid-cols-2 hover:bg-slate-700 px-2'>
+                                                                <p className='col-span-1 text-sm'>Storage</p>
+                                                                <p className='col-span-1 text-sm'>: {variant?.storage}</p>
+                                                            </div>}
+                                                            {variant?.weight && <div className='grid grid-cols-2 hover:bg-slate-700 px-2'>
+                                                                <p className='col-span-1 text-sm'>Weight</p>
+                                                                <p className='col-span-1 text-sm'>: {variant?.weight}</p>
+                                                            </div>}
+                                                            {variant?.dimensions && <div className='grid grid-cols-2 hover:bg-slate-700 px-2'>
+                                                                <p className='col-span-2 text-xs'>{variant?.dimensions}</p>
+                                                            </div>}
+                                                            {<div className='grid grid-cols-2 hover:bg-slate-700 px-2'>
+                                                                <p className='col-span-1 text-sm'>Stock</p>
+                                                                <p className='col-span-1 text-sm'>: {variant?.stock || "Out of stock"}</p>
+                                                            </div>}
+                                                        </div>
+                                                    </li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                }
+
+                                {product?.data && <div className="flex justify-end max-w-72">
+
+                                    <AddToCartButton product={product?.data} />
+
+                                </div>}
 
                             </div>
                         </div>
-                        <div
-                            className="col-span-12">
-                            <h1 className="text-xl font-bold text-slate-50 dark:text-slate-300 mb-5">Variants:</h1>
 
-                            <div className="overflow-x-auto">
-                                <table className="min-w-full table-auto border-collapse">
 
-                                    <thead>
-                                        <tr className="bg-gray-200 dark:bg-slate-700 text-left">
-                                            <th className="p-4 border dark:border-slate-700">Photo</th>
-                                            <th className="p-4 border dark:border-slate-700">Color</th>
-                                            <th className="p-4 border dark:border-slate-700">Size</th>
-                                            <th className="p-4 border dark:border-slate-700">Material</th>
-                                            <th className="p-4 border dark:border-slate-700">Price</th>
-                                            <th className="p-4 border dark:border-slate-700">Stock</th>
-                                            <th className="p-4 border dark:border-slate-700">Weight</th>
-                                            <th className="p-4 border dark:border-slate-700">Dimensions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {product?.data?.variant?.map((variant) => (
-                                            <tr key={variant.id} className="border-b">
-                                                <td className="p-4 border dark:border-slate-700">
-                                                    <div className="w-full flex justify-center ">
-                                                        <Image src={variant?.images?.[0]} width={50} height={50} alt={product?.data?.name} className='object-contain' />
-                                                    </div>
-                                                </td>
-                                                <td className="p-4 border dark:border-slate-700">{variant.color}</td>
-                                                <td className="p-4 border dark:border-slate-700">{variant.size}</td>
-                                                <td className="p-4 border dark:border-slate-700">{variant.material}</td>
-                                                <td className="p-4 border dark:border-slate-700">${variant.price.toFixed(2)}</td>
-                                                <td className="p-4 border dark:border-slate-700">{variant.stock}</td>
-                                                <td className="p-4 border dark:border-slate-700">{variant.weight} kg</td>
-                                                <td className="p-4 border dark:border-slate-700">{variant.dimensions}</td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
 
-                        </div>
-
-                        {product?.data && <div className="col-span-12 flex justify-end">
-
-                            <div className='max-w-72'>
-                                {/************************Save for later button ***********************/}
-                                <SaveForLaterButton product={product?.data} />
-
-                                {/************************Add to cart button ***********************/}
-                                <AddToCartButton product={product?.data} />
-                            </div>
-                        </div>}
                     </div>
 
 
