ab0ad0888a3d292223a8e76a3c468b0f31142730
rukonpro <rukon.js@gmail.com>
2024-12-13 02:16:00 +0600

    update product form

--- app/components/AdminDashboard/creatore/NewProductForm.js
+++ app/components/AdminDashboard/creatore/NewProductForm.js
@@ -8,8 +8,6 @@ import { CldUploadWidget } from 'next-cloudinary';
 import baseURL from '@/app/utils/baseURL';
 
 const ProductForm = () => {
-  const [tags, setTags] = useState('');
-
   const validationSchema = Yup.object({
     name: Yup.string().required('Name is required'),
     description: Yup.string().required('Description is required'),
@@ -27,26 +25,59 @@ const ProductForm = () => {
           name: Yup.string().required('Specification name is required'),
           value: Yup.string().required('Specification value is required')
         })
+      )
+      .test(
+        'unique-names',
+        'Specification names must be unique',
+        (specs) => {
+          const names = specs.map(spec => spec.name);
+          return new Set(names).size === names.length;
+        }
       ),
-    tags: Yup.array().of(Yup.string()).min(1, 'At least one tag is required'),
-    variants: Yup.array().of(
-      Yup.object().shape({
-        attributes: Yup.array().of(
-          Yup.object().shape({
-            attributeName: Yup.string().required('Attribute name is required'),
-            attributeValue: Yup.string().required('Attribute value is required')
-          })
-        ).min(1, 'At least one attribute is required for each variant'),
-        price: Yup.number()
-          .required('Price is required')
-          .positive('Price must be positive'),
-        stock: Yup.number()
-          .required('Stock is required')
-          .positive('Stock must be positive'),
-        availability: Yup.string().required('Availability is required'),
-        images: Yup.array().min(1, 'At least one image is required for the variant')
-      })
-    ).min(1, 'At least one variant is required')
+    tags: Yup.array()
+      .of(
+        Yup.string()
+          .required('Tag is required')
+          .min(1, 'Tag cannot be empty')
+          .trim('Tag cannot contain only spaces')
+      )
+      .min(1, 'At least one tag is required'),
+    variants: Yup.array()
+      .of(
+        Yup.object().shape({
+          attributes: Yup.array()
+            .of(
+              Yup.object().shape({
+                name: Yup.string()
+                // .required('Attribute name is required')
+                // .min(1, 'Attribute name cannot be empty')
+                ,
+                value: Yup.string()
+                // .required('Attribute value is required')
+                // .min(1, 'Attribute value cannot be empty')
+                ,
+              })
+            )
+            .min(1, 'At least one attribute is required')
+          ,
+          price: Yup.number()
+            .required('Price is required')
+            .positive('Price must be positive')
+          ,
+          stock: Yup.number()
+            .required('Stock is required')
+            .min(1, 'Stock must be at least 1')
+          ,
+          availability: Yup.string()
+            .required('Availability is required')
+          ,
+          images: Yup.array()
+            .of(Yup.string().url('Invalid image URL'))
+            .min(1, 'At least one image is required')
+          ,
+        })
+      )
+      .min(1, 'At least one variant is required')
   });
 
 
@@ -69,11 +100,7 @@ const ProductForm = () => {
     },
     validationSchema,
     onSubmit: (values) => {
-      console.log({
-        ...values,
-        tags: tags.split(','),
-       
-      });
+      console.log("submmit", values);
     },
   });
 
@@ -85,15 +112,15 @@ const ProductForm = () => {
     formik.setFieldValue("specifications", [...formik.values.specifications, { name: '', value: '' }]);
 
   };
-  console.log(formik.values.specifications)
-  const updateSpecification = (index, key, value) => {
-    const updatedFields = [...formik.values.specifications];
-    updatedFields[index][key] = value;
-    formik.setFieldValue("specifications", updatedFields);
-  };
+
 
   const removeSpecification = (index) => {
     formik.setFieldValue("specifications", formik.values.specifications.filter((_, i) => i !== index));
+  };
+
+  const removeImage = (index) => {
+    const updatedImages = formik.values.images.filter((_, i) => i !== index);
+    formik.setFieldValue('images', updatedImages);
   };
 
 
@@ -122,6 +149,7 @@ const ProductForm = () => {
     updatedVariants[index].images = [...updatedVariants[index].images, url];
     formik.setFieldValue("variants", updatedVariants);
   };
+
 
   return (
     <form onSubmit={formik.handleSubmit} className="p-8 max-w-4xl mx-auto">
@@ -179,31 +207,57 @@ const ProductForm = () => {
 
       <div>
         <h3 className="text-lg font-medium mb-2">Specifications</h3>
-        {formik.values?.specifications?.map((field, index) => (
-          <div key={index} className="flex gap-4 mb-3">
-            <input
-              type="text"
-              placeholder="Name"
-              value={field.name}
-              onChange={(e) => updateSpecification(index, 'name', e.target.value)}
-              className="border p-2 rounded w-full"
-            />
-            <input
-              type="text"
-              placeholder="Value"
-              value={field.value}
-              onChange={(e) => updateSpecification(index, 'value', e.target.value)}
-              className="border p-2 rounded w-full"
-            />
-            <button
-              type="button"
-              onClick={() => removeSpecification(index)}
-              className="bg-red-500 text-white px-3 rounded"
-            >
-              Remove
-            </button>
+        {formik.values.specifications.map((field, index) => (
+          <div key={index} className="grid grid-cols-12 gap-4 items-center mb-3 py-3 bg-slate-100">
+            <div className='col-span-5'>
+              {/* Field for Specification Name */}
+              <input
+                type="text"
+                placeholder="Name"
+                {...formik.getFieldProps(`specifications[${index}].name`)}
+                className="border p-2 rounded w-full"
+              />
+
+              {/* Error message for Name */}
+              {formik.touched.specifications?.[index]?.name &&
+                formik.errors.specifications?.[index]?.name && (
+                  <div className="text-red-500">
+                    {formik.errors.specifications[index].name}
+                  </div>
+                )}
+
+            </div>
+            <div className='col-span-5'>
+              {/* Field for Specification Value */}
+              <input
+                type="text"
+                placeholder="Value"
+                {...formik.getFieldProps(`specifications[${index}].value`)}
+                className="border p-2 rounded w-full"
+              />
+
+              {/* Error message for Value */}
+              {formik.touched.specifications?.[index]?.value &&
+                formik.errors.specifications?.[index]?.value && (
+                  <div className="text-red-500">
+                    {formik.errors.specifications[index].value}
+                  </div>
+                )}
+            </div>
+
+            <div className='col-span-2'>
+              {/* Remove Button */}
+              <button
+                type="button"
+                onClick={() => removeSpecification(index)}
+                className="bg-red-500 text-white px-3 rounded"
+              >
+                Remove
+              </button>
+            </div>
           </div>
         ))}
+        {/* Add Specification Button */}
         <button
           type="button"
           onClick={addSpecification}
@@ -213,13 +267,56 @@ const ProductForm = () => {
         </button>
       </div>
 
-      <input
-        type="text"
-        placeholder="Tags (comma-separated)"
-        value={tags}
-        onChange={(e) => setTags(e.target.value)}
-        className="border p-2 rounded w-full my-4"
-      />
+      <div>
+        <h3 className="text-lg font-medium mb-2">Tags</h3>
+        <div className="flex flex-col gap-2">
+          {formik.values.tags.map((tag, index) => (
+            <div key={index} className="grid grid-cols-6 gap-4 items-center">
+              <div className='col-span-5'>
+                {/* Input for Tag */}
+                <input
+                  type="text"
+                  placeholder={`Tag ${index + 1}`}
+                  {...formik.getFieldProps(`tags[${index}]`)}
+                  className="border p-2 rounded w-full"
+                />
+                <br />
+                {/* Error Message for Each Tag */}
+                {formik.touched.tags?.[index] && formik.errors.tags?.[index] && (
+                  <div className="text-red-500">{formik.errors.tags[index]}</div>
+                )}
+              </div>
+              {/* Remove Button */}
+              <button
+                type="button"
+                onClick={() =>
+                  formik.setFieldValue(
+                    'tags',
+                    formik.values.tags.filter((_, i) => i !== index)
+                  )
+                }
+                className="bg-red-500 text-white px-3 rounded className='col-span-1'"
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+        </div>
+
+        {/* Add New Tag Button */}
+        <button
+          type="button"
+          onClick={() => {
+            const updatedTags = [...formik.values.tags, ''];
+            formik.setFieldValue('tags', updatedTags);
+          }}
+          className="bg-blue-500 text-white px-4 py-2 rounded mt-3"
+        >
+          Add Tag
+        </button>
+      </div>
+
+
 
       <input
         type="text"
@@ -240,94 +337,130 @@ const ProductForm = () => {
       {formik.touched.categoryId && formik.errors.categoryId && (
         <div className="text-red-500">{formik.errors.categoryId}</div>
       )}
-
       <CldUploadWidget
         signatureEndpoint={`${baseURL}/api/signature/signature`}
-        onUpload={(result, { widget }) => {
-          formik.setFieldValue("images",[...formik.values?.images, result?.info?.secure_url]);
+        onUpload={(result) => {
+          console.log(result)
+          formik.setFieldValue('images', [...formik.values.images, result.info.secure_url]);
         }}
       >
         {({ open }) => {
-          function handleOnClick() {
-            open(); // Open the upload widget
-          }
+
           return (
             <button
-              type='button'
-              onClick={handleOnClick} className="px-5 flex items-center justify-center gap-2 rounded-full border-2 dark:bg-slate-700 dark:border-slate-600 dark:text-slate-300">
+              type="button"
+              onClick={() => open()}
+              className="px-4 py-2 flex items-center gap-2 rounded-full border bg-gray-100 hover:bg-gray-200"
+            >
               <RiImageAddFill />
-              Upload
+              Upload Image
             </button>
           );
         }}
       </CldUploadWidget>
 
 
-      <div className="grid grid-cols-3 gap-4">
+      <div className="flex flex-wrap gap-2 mt-2">
         {formik.values?.images?.map((image, index) => (
-          <div key={index} className="border p-2 rounded">
-
-            <Image src={image} width={200} height={200} alt={`Preview ${index}`} className="w-full h-32 object-cover" />
+          <div key={index} className="relative border rounded">
+            <Image src={image} width={200} height={200} alt={`Preview ${index}`} className="w-20 h-20 object-cover rounded" />
+            <button
+              type="button"
+              onClick={() => removeImage(index)}
+              className="absolute top-0 right-0 bg-red-500 text-white rounded-full px-2 py-1 text-xs"
+            >
+               ×
+            </button>
           </div>
         ))}
       </div>
 
-
+      {formik.touched.images && formik.errors.images && (
+        <div className="text-red-500">{formik.errors.images}</div>
+      )}
 
       {/************************* variants  *************************/}
 
       <div>
         <h3 className="text-lg font-medium mb-2">Variants</h3>
-        {formik?.values?.variants?.map((variant, index) => (
+        {formik.values.variants.map((variant, index) => (
           <div key={index} className="mb-4 border p-4 rounded">
             <h4 className="font-medium mb-2">Variant {index + 1}</h4>
+
+            {/* Attributes Section */}
             <DynamicFields
               fields={variant.attributes}
               setFields={(fields) => updateVariant(index, 'attributes', fields)}
               title="Attributes"
             />
+
+            {/* Price Input */}
             <input
               type="number"
               placeholder="Price"
-              value={variant.price}
-              onChange={(e) => updateVariant(index, 'price', e.target.value)}
+              {...formik.getFieldProps(`variants[${index}].price`)}
               className="border p-2 rounded w-full my-2"
             />
+            {formik.touched.variants?.[index]?.price &&
+              formik.errors.variants?.[index]?.price && (
+                <div className="text-red-500">{formik.errors.variants[index].price}</div>
+              )}
+
+            {/* Stock Input */}
             <input
               type="number"
               placeholder="Stock"
-              value={variant.stock}
-              onChange={(e) => updateVariant(index, 'stock', e.target.value)}
+              {...formik.getFieldProps(`variants[${index}].stock`)}
               className="border p-2 rounded w-full my-2"
             />
+            {formik.touched.variants?.[index]?.stock &&
+              formik.errors.variants?.[index]?.stock && (
+                <div className="text-red-500">{formik.errors.variants[index].stock}</div>
+              )}
+
+            {/* Availability Dropdown */}
             <select
-              value={variant.availability}
-              onChange={(e) => updateVariant(index, 'availability', e.target.value)}
+              {...formik.getFieldProps(`variants[${index}].availability`)}
               className="border p-2 rounded w-full my-2"
             >
               <option value="IN_STOCK">In Stock</option>
               <option value="OUT_OF_STOCK">Out of Stock</option>
             </select>
+            {formik.touched.variants?.[index]?.availability &&
+              formik.errors.variants?.[index]?.availability && (
+                <div className="text-red-500">
+                  {formik.errors.variants[index].availability}
+                </div>
+              )}
 
+            {/* Variant Images */}
             <div className="mb-2">
               <h5 className="font-medium">Images</h5>
+
+
               <CldUploadWidget
                 signatureEndpoint={`${baseURL}/api/signature/signature`}
-                onUpload={(result) => handleVariantImageUpload(index, result?.info?.secure_url)}
+                onUpload={(result) => {
+                  handleVariantImageUpload(index, result.info.secure_url)
+                }}
               >
-                {({ open }) => (
-                  <button
-                    type="button"
-                    onClick={open}
-                    className="px-4 py-2 flex items-center gap-2 rounded-full border bg-gray-100 hover:bg-gray-200"
-                  >
-                    <RiImageAddFill />
-                    Upload Image
-                  </button>
-                )}
+                {({ open }) => {
+
+                  return (
+                    <button
+                      type="button"
+                      onClick={() => open()}
+                      className="px-4 py-2 flex items-center gap-2 rounded-full border bg-gray-100 hover:bg-gray-200"
+                    >
+                      <RiImageAddFill />
+                      Upload Image
+                    </button>
+                  );
+                }}
               </CldUploadWidget>
             </div>
 
+            {/* Display Uploaded Images */}
             <div className="flex flex-wrap gap-2 mt-2">
               {variant.images.map((image, imgIndex) => (
                 <div key={imgIndex} className="relative">
@@ -341,7 +474,9 @@ const ProductForm = () => {
                   <button
                     type="button"
                     onClick={() => {
-                      const updatedImages = variant.images.filter((_, i) => i !== imgIndex);
+                      const updatedImages = variant.images.filter(
+                        (_, i) => i !== imgIndex
+                      );
                       updateVariant(index, 'images', updatedImages);
                     }}
                     className="absolute top-0 right-0 bg-red-500 text-white rounded-full px-2 py-1 text-xs"
@@ -351,7 +486,13 @@ const ProductForm = () => {
                 </div>
               ))}
             </div>
+            {/* Error Message for Images */}
+            {formik.touched.variants?.[index]?.images &&
+              formik.errors.variants?.[index]?.images && (
+                <div className="text-red-500">{formik.errors.variants[index].images}</div>
+              )}
 
+            {/* Remove Variant Button */}
             <button
               type="button"
               onClick={() => removeVariant(index)}
@@ -361,6 +502,8 @@ const ProductForm = () => {
             </button>
           </div>
         ))}
+
+        {/* Add Variant Button */}
         <button
           type="button"
           onClick={addVariant}
@@ -369,6 +512,7 @@ const ProductForm = () => {
           Add Variant
         </button>
       </div>
+
 
       <button
         type="submit"
